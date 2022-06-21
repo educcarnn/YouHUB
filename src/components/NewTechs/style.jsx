c244d3991f5f58a919cddc3cf0dc6830740f04fa
Eduardo <educcarnn@gmail.com>
2022-06-21 16:34:05 -0300

    Feature: Init style mobile

--- src/components/NewTechs/style.jsx
+++ src/components/NewTechs/style.jsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 export const AddStyle = styled.div`
-    background-color: black;
-    width: 80%;
-    height: 80%;
+   background-color: black;
+    width: 88%;
+    height: 20%;
+    position: fixed;
+    flex-direction: column;
 `
